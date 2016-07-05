9843fd16f96dedfb6a9635c7ad2306217bc6ad06
André Skoglund Hansen <andre.ok@online.no>
2016-07-06 01:38:35 +0200

    MainScene cleanup and RTSCamera disabled edge scrolling

--- Assets/UI/RTSCamera.cs
+++ Assets/UI/RTSCamera.cs
@@ -24,7 +24,7 @@ public class RTSCamera : MonoBehaviour
         if (!KeyboardManager.KeyboardLock)
             MoveCameraWithKeyboard();
 
-        MoveCameraWithMouseAndScreenEdge();
+        //MoveCameraWithMouseAndScreenEdge();
         ZoomCameraWithScrollWheel();
     }
 
