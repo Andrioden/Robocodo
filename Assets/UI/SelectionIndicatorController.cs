162214bd13a4e909b9e8fbe7e7863d5cdbba77d1
Bjørn Tore Gjerde <btgjerde@gmail.com>
2016-07-13 13:13:03 +0200

    Selection indiator.
    Missing  <Compile Include="Assets\UI\SelectionIndicatorController.cs" />

---
+++ Assets/UI/SelectionIndicatorController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionIndicatorController : MonoBehaviour
+{
+    public GameObject quad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MouseManager.currentlySelected != null)
+        {
+            quad.SetActive(true);
+            transform.position = MouseManager.currentlySelected.transform.position;
+        }
+        else
+            quad.SetActive(false);
+    }
+}
