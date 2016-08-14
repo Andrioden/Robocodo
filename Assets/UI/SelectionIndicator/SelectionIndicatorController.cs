8cf0ca6fe314e5deb3bd5f92c7d78368fd8d6d3a
Bjørn Tore Gjerde <btgjerde@gmail.com>
2016-08-14 18:48:11 +0200

    Scale selectionIndicator based on isRobot closes #61

--- Assets/UI/SelectionIndicator/SelectionIndicatorController.cs
+++ Assets/UI/SelectionIndicator/SelectionIndicatorController.cs
@@ -1,19 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SelectionIndicatorController : MonoBehaviour
 {
     public GameObject quad;
+    Vector3 defaultScale;
+    float robotScaleFactor = 0.8f;
+    GameObject lastSelection = null;
 
-    // Update is called once per frame
+    void Awake()
+    {
+        defaultScale = transform.localScale;
+    }
+
     void Update()
     {
         if (MouseManager.currentlySelected != null)
         {
             quad.SetActive(true);
             transform.position = MouseManager.currentlySelected.transform.position;
+            SetScale();
         }
         else
             quad.SetActive(false);
+    }
+
+    private void SetScale()
+    {
+        if (lastSelection == MouseManager.currentlySelected)
+            return;
+        else
+            lastSelection = MouseManager.currentlySelected;
+
+        bool isRobot = false;
+        var robotComponent = MouseManager.currentlySelected.GetComponent<RobotController>();
+        if (robotComponent != null)
+            isRobot = true;
+
+        var scaleFactor = isRobot ? robotScaleFactor : 1f;
+        transform.localScale = new Vector3(defaultScale.x * scaleFactor, defaultScale.y * scaleFactor, defaultScale.z * scaleFactor);
     }
 }
