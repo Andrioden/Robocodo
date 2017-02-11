c4f8e8e2ee2a91e34dd454fde743c86fa629fd1a
Bjørn Tore Gjerde <btgjerde@gmail.com>
2017-02-11 15:27:47 +0100

    Added new transporter robot mesh

---
+++ Assets/Robots/Transporter/Scripts/Roll_Wheels.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var wheelDiameter :float = 1;
+private var wheelLength:float;
+private var oldPosition:Vector3;
+private var newPosition:Vector3;
+private var distance: float;
+
+function Start () {
+
+oldPosition = new Vector3(0,0,0);
+wheelLength = wheelDiameter*Mathf.PI;
+
+}
+
+function Update () {
+
+newPosition = this.transform.position;
+distance = Vector3.Distance(oldPosition, newPosition);
+oldPosition = newPosition;
+
+this.transform.Rotate(distance/wheelLength*360,0,0);
+}
