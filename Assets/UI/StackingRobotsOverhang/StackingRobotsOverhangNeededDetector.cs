bab2e27c59ff60fb03d6b66722fb490d26a2ad43
André Skoglund Hansen <andre.ok@online.no>
2017-02-18 17:27:55 +0100

    Stacking robot detections to be moved to collision detected #191

---
+++ Assets/UI/StackingRobotsOverhang/StackingRobotsOverhangNeededDetector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackingRobotsOverhangNeededDetector : MonoBehaviour
+{
+
+    RobotController robot;
+
+    // Use this for initialization
+    private void Start()
+    {
+        robot = GetComponentInParent<RobotController>();
+
+        if (robot == null)
+            throw new Exception("Could not find RobotController in parent, have you changed the structure of the robot prefab?");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Enter for " + robot.name);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exit for " + robot.name);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Staying for " + robot.name);
+    }
+}
